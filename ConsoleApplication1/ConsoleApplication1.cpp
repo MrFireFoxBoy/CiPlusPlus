861066594b78011e4acda08d2cd77bcd6cd648f7
MrFireFoxBoy <65604905+MrFireFoxBoy@users.noreply.github.com>
2023-12-26 21:44:41 +0300

    Add files via upload

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,32 +1,66 @@
 ﻿#include <iostream>
-#include <stdlib.h>
-const int n = 3;
+#include "sqlite3.h"
 
-int main()
-{
-	int mass[n + 1] = { 1,2,3,4 };
-	int k = 0;
-	int skip = 1;
+// Функция обратного вызова для вывода результатов запроса
+static int callback(void* data, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++) {
+        std::cout << azColName[i] << ": " << argv[i] << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
+}
 
-	for (int i = 0; i <= n; i++)
-	{
-		printf("%d\n", mass[i]);
-	}
-	for (int i = 0; i < skip; i++)
-	{
-		k = mass[n];
+int main() {
+    // Подключение к базе данных
+    sqlite3* db;
+    int rc = sqlite3_open("test.db", &db);
 
-		for (int j = n; j >= 0; j--)
-		{
-			mass[j] = mass[j - 1];
-		}
-		mass[0] = k;
-	}
+    if (rc) {
+        std::cout << "Error opening the database: " << sqlite3_errmsg(db) << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "The database has been opened successfully" << std::endl;
+    }
 
-	printf("result \n");
+    // Создание таблицы, если она не существует
+    std::string createTableQuery = "CREATE TABLE IF NOT EXISTS MyTable (Name TEXT, Age INT);";
+    rc = sqlite3_exec(db, createTableQuery.c_str(), NULL, 0, NULL);
 
-	for (int i = 0; i <= n; i++)
-	{
-		printf("%d\n", mass[i]);
-	}
+    if (rc != SQLITE_OK) {
+        std::cout << "Error creating the table: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 0;
+    }
+    else {
+        std::cout << "The table was created successfully" << std::endl;
+    }
+
+    // Добавление данных в таблицу
+    std::string insertQuery = "INSERT INTO MyTable (Name, Age) VALUES ('John Doe', 25);";
+    rc = sqlite3_exec(db, insertQuery.c_str(), NULL, 0, NULL);
+
+    if (rc != SQLITE_OK) {
+        std::cout << "Error when adding data to the table: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 0;
+    }
+    else {
+        std::cout << "The data has been added successfully" << std::endl;
+    }
+
+    // Выполнение запроса на выборку данных
+    std::string selectQuery = "SELECT * FROM MyTable;";
+    rc = sqlite3_exec(db, selectQuery.c_str(), callback, 0, NULL);
+
+    if (rc != SQLITE_OK) {
+        std::cout << "Error when executing a data selection request: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 0;
+    }
+
+    // Закрытие соединения с базой данных
+    sqlite3_close(db);
+
+    return 0;
 }
